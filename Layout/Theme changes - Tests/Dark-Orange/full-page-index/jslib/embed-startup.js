461c1552a6ab8f69c8eda487355b7ec0ecb730f2
Piotr Szymaniec <traf@angrenost.org>
2020-09-13 23:17:31 +0200

    reshuffling

--- Layout/Theme changes - Tests/Dark-Orange/full-page-index/jslib/embed-startup.js
+++
@@ -1,5 +0,0 @@
-
-
-console.log("Embed Startup start")
-EmbedMain().main()
-console.log("Embed Startup end")
--- UI_Design/Theme changes - Tests/test tmpLooty!_files/startup.js
+++
@@ -1,5 +0,0 @@
-//ScalaJS.modules.example_ScalaJSExample().main();
-
-console.log("Startup.js start")
-LootyMain().main()
-console.log("Startup.js end")
